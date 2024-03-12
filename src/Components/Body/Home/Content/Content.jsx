72883ae46e803878e6e1c1af4ad92f0eaf9bb045
praveen-raj-143 <praveenrajgreath@gmail.com>
2024-03-13 00:11:41 +0530

    completed

--- src/Components/Body/Home/Content/Content.jsx
+++ src/Components/Body/Home/Content/Content.jsx
@@ -14,14 +14,17 @@ const Content = () => {
         <div className='contentimg'>
             <div className='contenttext'>
             <h1>Vamsi & Meghana</h1>
-            <p>WEDNESDAY, MARCH 13 2024</p>
-            <button className='morebtn' onClick={morecontent}>VIEW GALLERY</button>
+            <p className='subtitle'>WEDNESDAY, MARCH 13 2024</p>
+            
             <div className='contenttextt'>
             <p>Please click the following Button to watch live. <Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing"><button className='livebtn'>live</button></Link></p>
-            <p>Click here to watch <Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing"><span>Live Photos</span></Link></p>
+            <p><Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing">Click here to watch <span>Live Photos</span></Link></p>
+            <button className='morebtn' onClick={morecontent}>VIEW GALLERY</button>
             </div>
+            
             </div>
         </div>
+        
         {click ? "" : <Morepics/>}
     </div>
   )
