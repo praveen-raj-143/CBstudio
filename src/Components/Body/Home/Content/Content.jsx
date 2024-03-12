2f4a19d2452db78d51148dd45fafcfd4d2c310f9
praveen-raj-143 <praveenrajgreath@gmail.com>
2024-03-13 00:20:38 +0530

    completed

--- src/Components/Body/Home/Content/Content.jsx
+++ src/Components/Body/Home/Content/Content.jsx
@@ -17,7 +17,7 @@ const Content = () => {
             <p className='subtitle'>WEDNESDAY, MARCH 13 2024</p>
             
             <div className='contenttextt'>
-            <p>Please click the following Button to watch live. <Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing"><button className='livebtn'>live</button></Link></p>
+            <p>Please click the following Button to watch live. <Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing"><button className='livebtn'>Live</button></Link></p>
             <p><Link className='live' to="https://drive.google.com/drive/folders/1HxkdukxLs1ZoC4xfjQELHPLhN9wzhif3?usp=sharing">Click here to watch <span>Live Photos</span></Link></p>
             <button className='morebtn' onClick={morecontent}>VIEW GALLERY</button>
             </div>
